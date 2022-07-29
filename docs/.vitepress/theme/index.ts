ae403428cccf262a1ea3a500fd979eeb9133cde1
tyh2001 <1469442737@qq.com>
2022-07-29 15:40:34 +0800

    chore: 更新配置项

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,12 +5,12 @@ import DefaultTheme from 'vitepress/theme'
  * 组件库部分功能尚未完善，仅供测试使用
  * 详情参考：https://github.com/FightingDesign/fighting-design
  */
-import FightingDesign from 'fighting-design'
-import 'fighting-design/theme/style.css'
+// import FightingDesign from 'fighting-design'
+// import 'fighting-design/theme/style.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.use(FightingDesign)
+    // app.use(FightingDesign)
   }
 }
