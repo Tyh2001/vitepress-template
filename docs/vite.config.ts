e0b4a4a312639cbf37070b49954574f28974ca73
15227241216 <1469442737@qq.com>
2022-08-18 18:51:49 +0800

    chore: 更新配置文件

---
+++ docs/vite.config.ts
@@ -0,0 +1,25 @@
+import type { UserConfigExport } from 'vite'
+
+export default (): UserConfigExport => {
+  return {
+    optimizeDeps: {
+      exclude: ['vitepress']
+    },
+    css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove()
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
