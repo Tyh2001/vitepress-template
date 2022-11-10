af9dab670283915a524a7f3479da6d37e3319cda
tyh2001 <1469442737@qq.com>
2022-11-10 21:28:56 +0800

    fest: 新增导航栏搜索框

---
+++ docs/.vitepress/theme/components/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <f-input v-model="value1" type="text" placeholder="请输入...." />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const value1 = ref('')
+</script>
+
+
+<style lang="scss" scoped>
+.f-input {
+  width: 200px;
+}
+</style>
+
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
-
+import { h } from 'vue'
+import Search from './components/Search.vue'
 import './style/index.scss'
 
 /**
@@ -11,6 +12,17 @@ import 'fighting-design/dist/index.css'
 
 export default {
   ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      /**
+       * 导航栏插入搜索的输入框插槽
+       * 更多插槽参考
+       * 
+       * https://github.com/vuejs/vitepress/blob/main/docs/guide/theme-introduction.md 
+       */
+      'nav-bar-content-before': () => h(Search)
+    })
+  },
   enhanceApp({ app }) {
     app.use(FightingDesign)
   }
