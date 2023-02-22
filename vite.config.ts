954b9d5d96d997f442908cc6a4734a69daf4b25b
tyh2001 <1469442737@qq.com>
2023-02-22 09:46:48 +0800

    chore: 更新 vue 版本

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,22 @@ import type { UserConfigExport, Plugin } from 'vite'
  * @see issues#1015 https://github.com/vuejs/vitepress/discussions/1015#discussioncomment-3177860
  * @returns { Object }
  */
-const NavLinkPatch = (): Plugin => ({
-  name: 'override-target-blank',
-  enforce: 'pre',
-  transform: (code, id) => {
-    if (id.endsWith('VPLink.vue')) {
-      return code.replace('_blank', '_self')
-    }
-  }
-})
+// const NavLinkPatch = (): Plugin => ({
+//   name: 'override-target-blank',
+//   enforce: 'pre',
+//   transform: (code, id) => {
+//     if (id.endsWith('VPLink.vue')) {
+//       return code.replace('_blank', '_self')
+//     }
+//   }
+// })
 
 export default (): UserConfigExport => {
   return {
     server: {
       port: 1216
     },
-    plugins: [NavLinkPatch()],
+    // plugins: [NavLinkPatch()],
     optimizeDeps: {
       exclude: ['vitepress']
     },
@@ -31,21 +31,21 @@ export default (): UserConfigExport => {
     legacy: {
       buildSsrCjsExternalHeuristics: true
     },
-    css: {
-      postcss: {
-        plugins: [
-          {
-            postcssPlugin: 'internal:charset-removal',
-            AtRule: {
-              charset: (atRule) => {
-                if (atRule.name === 'charset') {
-                  atRule.remove()
-                }
-              }
-            }
-          }
-        ]
-      }
-    }
+    // css: {
+    //   postcss: {
+    //     plugins: [
+    //       {
+    //         postcssPlugin: 'internal:charset-removal',
+    //         AtRule: {
+    //           charset: (atRule) => {
+    //             if (atRule.name === 'charset') {
+    //               atRule.remove()
+    //             }
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   }
+    // }
   }
 }
