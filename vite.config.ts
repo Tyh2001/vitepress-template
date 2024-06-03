d7cb94330a83992f2d7d2fe2e849b1e52f5d1bc2
Tyh2001 <1469442737@qq.com>
2024-06-03 18:24:42 +0800

    chore：更新配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,13 @@
-import type { UserConfigExport, Plugin } from 'vite'
+import type { UserConfigExport } from 'vite'
 
-/**
- * 解决 target = "_self" on nav links 报错问题
- * 
- * @see issues#1015 https://github.com/vuejs/vitepress/discussions/1015#discussioncomment-3177860
- * @returns { Object }
- */
-// const NavLinkPatch = (): Plugin => ({
-//   name: 'override-target-blank',
-//   enforce: 'pre',
-//   transform: (code, id) => {
-//     if (id.endsWith('VPLink.vue')) {
-//       return code.replace('_blank', '_self')
-//     }
-//   }
-// })
 
 export default (): UserConfigExport => {
   return {
     server: {
       port: 1216
     },
-    // plugins: [NavLinkPatch()],
     optimizeDeps: {
       exclude: ['vitepress']
     },
-    ssr: {
-      format: 'cjs'
-    },
-    legacy: {
-      buildSsrCjsExternalHeuristics: true
-    },
-    // css: {
-    //   postcss: {
-    //     plugins: [
-    //       {
-    //         postcssPlugin: 'internal:charset-removal',
-    //         AtRule: {
-    //           charset: (atRule) => {
-    //             if (atRule.name === 'charset') {
-    //               atRule.remove()
-    //             }
-    //           }
-    //         }
-    //       }
-    //     ]
-    //   }
-    // }
   }
 }
